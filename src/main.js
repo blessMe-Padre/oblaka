dc07071542e69febb66de7e5fe25efdf7f538a50
Сергей Субботин <zarodiny@yandex.ru>
2023-09-01 09:45:33 +1000

    feat: добавляет слайдер

--- src/main.js
+++ src/main.js
@@ -108,10 +108,36 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    //     // slidesPerColumn: 1,
-    //     // centeredSlides: true,
-
     //слайдер "Наши КЛИЕНТЫ"
+
+    const clients = document.querySelector('.clients');
+    if (clients) {
+        const swiper = new Swiper('.clients-swiper', {
+            loop: true,
+            spaceBetween: 20,
+            slidesPerView: 1,
+            breakpoints: {
+                580: {
+                    slidesPerView: 1.5,
+                    spaceBetween: 20,
+                },
+                767: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1023: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                    centeredSlides: true,
+                },
+            },
+
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
 
 
     // DOMContentLoaded
