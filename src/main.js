4fe6755ebfcccc2f2b34f8a49f9f8075fc700f11
Сергей Субботин <zarodiny@yandex.ru>
2023-09-01 13:10:06 +1000

    fix: баги

--- src/main.js
+++ src/main.js
@@ -139,6 +139,32 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // Плавный скролл до блока
+    const menuLinks = document.querySelectorAll('.nav-link');
+    if (menuLinks) {
+        const onMenuLinkClick = (evt) => {
+            const menuLink = evt.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: 'smooth',
+                });
+                evt.preventDefault();
+            }
+        };
+
+        if (menuLinks.length > 0) {
+            menuLinks.forEach((menuLink) => {
+                menuLink.addEventListener('click', onMenuLinkClick);
+            });
+        }
+
+    }
+
+
 
     // DOMContentLoaded
 });
